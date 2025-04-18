26e8a1e692e2d68d52544c9fa4a7d0b908243d6f
tienthanh47 <2023163ntto@gmail.com>
2025-04-18 16:10:54 +0700

    xong

---
+++ demo-3/database/seeders/UserSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    const MAX_RECORDS = 100;
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Truncate table
+        DB::table('users')->truncate();
+
+        // Insert admin user
+        DB::table('users')->insert([
+            [
+                'name' => 'admin',
+                'email' => 'admin@gmail.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('123456'),
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
+
+        // Insert additional users
+        for ($i = 1; $i < self::MAX_RECORDS; $i++) {
+            DB::table('users')->insert([
+                [
+                    'name' => 'user' . $i,
+                    'email' => "admin{$i}@gmail.com",
+                    'email_verified_at' => now(),
+                    'password' => Hash::make('123456'),
+                    'remember_token' => Str::random(10),
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]
+            ]);
+        }
+    }
+}
